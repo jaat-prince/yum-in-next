7a5f6efbe375ef3a6284bfc67d2741d2a14fc948
jaat-prince <princetetarwal17@gmail.com>
2025-02-12 15:01:54 +0530

    change in dashboard

--- src/app/components/common/Dashbr.jsx
+++ src/app/components/common/Dashbr.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Dashbr({ title, subtitle, text, tclass }) {
   return (
-    <div className="border border-[#e2e8f0] p-4 rounded-lg hover:border-[#f58220] w-full sm:max-w-[400px] transition-all ease-in-out duration-500">
+    <div className="border border-[#e2e8f0] p-4 rounded-lg hover:border-[#f58220] w-full sm:max-w-[400px] transition-all ease-in-out duration-500 flex flex-col justify-between">
       <h1 className="text-2xl mb-2 text-[#F58220] font-semibold text-center">
         {title}
       </h1>
